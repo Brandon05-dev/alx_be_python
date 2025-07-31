0d95eaad6162f3c0a9222d3bb4ea541d7fbed027
Brandon <brandonomutiti@gmail.com>
2025-07-31 18:28:51 +0300

    Feat: done

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,11 +6,11 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_addition(self):   # <-- Must match!
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):  # ✅ Fixed name!
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
